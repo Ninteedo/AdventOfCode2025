1ee9acf804ef32ce556b283cad05f9e0ff215023
Richard Holmes <richard.gordon.holmes@gmail.com>
2025-12-02 19:55:33 +0000

    Update Point2D and Point3D

--- src/main/scala/utility/Point2D.scala
+++ src/main/scala/utility/Point2D.scala
@@ -1,19 +1,19 @@
 package utility
 
 case class Point2D(x: Int, y: Int) {
-  def +(other: Point2D) = new Point2D(x + other.x, y + other.y)
+  def +(other: Point2D) = Point2D(x + other.x, y + other.y)
 
   def +(direction: Direction): Point2D = this + direction.toPoint
 
-  def -(other: Point2D) = new Point2D(x - other.x, y - other.y)
+  def -(other: Point2D) = Point2D(x - other.x, y - other.y)
 
   def -(direction: Direction): Point2D = this - direction.toPoint
 
-  def *(other: Point2D) = new Point2D(x * other.x, y * other.y)
+  def *(other: Point2D) = Point2D(x * other.x, y * other.y)
 
-  def *(factor: Int) = new Point2D(x * factor, y * factor)
+  def *(factor: Int) = Point2D(x * factor, y * factor)
 
-  def /(divisor: Int) = new Point2D(x / divisor, y / divisor)
+  def /(divisor: Int) = Point2D(x / divisor, y / divisor)
 
   def dotProduct(other: Point2D): Int = x * other.x + y * other.y
 
@@ -24,33 +24,22 @@ case class Point2D(x: Int, y: Int) {
 
   def transpose: Point2D = Point2D(y, x)
 
-  def canEqual(other: Any): Boolean = other.isInstanceOf[Point2D]
-
-  override def equals(other: Any): Boolean = other match {
-    case that: Point2D =>
-      x == that.x &&
-        y == that.y
-    case _ => false
-  }
-
-  override def hashCode(): Int = {
-    x * 31 + y
-  }
-
-  override def toString = s"Point2D($x, $y)"
+  override def toString = s"($x, $y)"
 }
 
 object Point2D {
-  val zero: Point2D = new Point2D(0, 0)
+  def apply(pair: (Int, Int)): Point2D = Point2D(pair._1, pair._2)
 
-  val directions: LazyList[Point2D] = LazyList(xVec(1), xVec(-1), yVec(1), yVec(-1))
+  val ZERO: Point2D = Point2D(0, 0)
 
-  val adjacents: LazyList[Point2D] = LazyList(
+  val DIRECTIONS: LazyList[Point2D] = LazyList(xVec(1), xVec(-1), yVec(1), yVec(-1))
+
+  val ADJACENTS: LazyList[Point2D] = LazyList(
     Point2D(-1, -1), Point2D(-1, 0), Point2D(-1, 1), Point2D(0, -1),
     Point2D(0, 1), Point2D(1, -1), Point2D(1, 0), Point2D(1, 1)
   )
 
-  def xVec(x: Int): Point2D = new Point2D(x, 0)
+  def xVec(x: Int): Point2D = Point2D(x, 0)
 
-  def yVec(y: Int): Point2D = new Point2D(0, y)
+  def yVec(y: Int): Point2D = Point2D(0, y)
 }
--- src/main/scala/utility/Point3D.scala
+++ src/main/scala/utility/Point3D.scala
@@ -1,44 +1,35 @@
 package utility
 
-class Point3D(val x: Int, val y: Int, val z: Int) {
-  def +(other: Point3D) = new Point3D(x + other.x, y + other.y, z + other.z)
+case class Point3D(x: Int, y: Int, z: Int) {
+  def +(other: Point3D) = Point3D(x + other.x, y + other.y, z + other.z)
 
-  def -(other: Point3D) = new Point3D(x - other.x, y - other.y, z - other.z)
+  def -(other: Point3D) = Point3D(x - other.x, y - other.y, z - other.z)
 
-  def *(factor: Int) = new Point3D(x * factor, y * factor, z * factor)
+  def *(factor: Int) = Point3D(x * factor, y * factor, z * factor)
 
-  def /(divisor: Int) = new Point3D(x / divisor, y / divisor, z / divisor)
+  def /(divisor: Int) = Point3D(x / divisor, y / divisor, z / divisor)
 
   def mannDist(other: Point3D): Int = (x - other.x).abs + (y - other.y).abs + (z - other.z).abs
 
-  def inVolume(vertex1: Point3D, vertex2: Point3D): Boolean =
-    vertex1.x <= x && x <= vertex2.x && vertex1.y <= y && y <= vertex2.y && vertex1.z <= z && z <= vertex2.z
-
-  def canEqual(other: Any): Boolean = other.isInstanceOf[Point3D]
-
-  override def equals(other: Any): Boolean = other match {
-    case that: Point3D =>
-        x == that.x &&
-        y == that.y &&
-        z == that.z
-    case _ => false
+  def inVolume(vertex1: Point3D, vertex2: Point3D): Boolean = {
+    x.inRangeInclusive(vertex1.x, vertex2.x) &&
+      y.inRangeInclusive(vertex1.y, vertex2.y) &&
+      z.inRangeInclusive(vertex1.z, vertex2.z)
   }
 
-  override def hashCode(): Int = {
-    x * 961 + y * 31 + z
-  }
-
-  override def toString = s"Point3D($x, $y, $z)"
+  override def toString = s"($x, $y, $z)"
 }
 
 object Point3D {
-  val zero: Point3D = new Point3D(0, 0, 0)
+  def apply(triple: (Int, Int, Int)): Point3D = Point3D(triple._1, triple._2, triple._3)
 
-  val directions: LazyList[Point3D] = LazyList(xVec(1), xVec(-1), yVec(1), yVec(-1), zVec(1), zVec(-1))
+  val ZERO: Point3D = Point3D(0, 0, 0)
 
-  def xVec(x: Int): Point3D = new Point3D(x, 0, 0)
+  val DIRECTIONS: LazyList[Point3D] = LazyList(xVec(1), xVec(-1), yVec(1), yVec(-1), zVec(1), zVec(-1))
 
-  def yVec(y: Int): Point3D = new Point3D(0, y, 0)
+  def xVec(x: Int): Point3D = Point3D(x, 0, 0)
 
-  def zVec(z: Int): Point3D = new Point3D(0, 0, z)
+  def yVec(y: Int): Point3D = Point3D(0, y, 0)
+
+  def zVec(z: Int): Point3D = Point3D(0, 0, z)
 }
